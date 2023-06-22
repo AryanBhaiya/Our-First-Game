d00dc85bfa7555a8df43b1ebf99445f11793442c
AryanBhaiya <128392783+AryanBhaiya@users.noreply.github.com>
2023-06-22 16:51:14 +0530

    update by aryan 5

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -11,13 +11,12 @@ public class CarController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
     }
 
     void Move()

2fc8306337c0a029a6f59a5bd1247aaf5a89d25f
RDXAISHWARY <aishwary.technologies@gmail.com>
2023-06-22 18:25:11 +0530

    upd

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -5,25 +5,39 @@ using UnityEngine;
 public class CarController : MonoBehaviour
 {
     public float moveSpeed;
-
     bool faceLeft, firstTab;
 
-<<<<<<< Updated upstream
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
-=======
->>>>>>> Stashed changes
-
-    // Update is called once per frame
+ // Update is called once per frame
     void Update()
     {
         Move();
+        CheckInput();
     }
 
     void Move()
     {
         transform.position += transform.forward * moveSpeed * Time.deltaTime;
+    }
+    
+    void CheckInput()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            ChangeDir()
+        }
+    }
+
+    void ChangeDir()
+    {
+            if(faceLeft)
+            {
+                faceleft = false;
+                transform.rotation = Quaternion.Evler(0, 90, 0);
+            }
+            else
+            {
+                faceLeft = true;
+                transform.rotation = Quaternion.Evler(0, 0, 0);
+            }
     }
 }

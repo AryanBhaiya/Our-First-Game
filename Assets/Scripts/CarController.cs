348667d462891f65cc569b0e98ac7abd60d3dab0
AryanBhaiya <128392783+AryanBhaiya@users.noreply.github.com>
2023-06-22 16:43:33 +0530

    script car forward

---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    public float moveSpeed;
+
+    bool faceLeft, dirstTab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Move()
+    {
+        transform.position += transform.forward * moveSpeed * Time.deltaTime;
+    }
+}

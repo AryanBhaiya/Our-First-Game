4c21bc146b8687b7c6bed7cf3eed57c87da3a24e
AryanBhaiya <128392783+AryanBhaiya@users.noreply.github.com>
2023-06-23 10:39:13 +0530

    Update By Aryan 1
    Managed To Set Directional Light
    
    Camera Position Change
    
    Almost Completed First Part

--- Assets/Scripts/CAMFOLLOWER.cs
+++ Assets/Scripts/CAMFOLLOWER.cs
@@ -2,30 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CamController : MonoBehaviour
+public class CAMFOLLOWER : MonoBehaviour
 {
 
     public Transform target;
     Vector3 distance;
+
     public float followSpeed;
 
     // Start is called before the first frame update
     void Start()
     {
         distance = target.position - transform.position;
-    }
+    }//start
 
     // Update is called once per frame
     void Update()
     {
         Follow();
     }
+    
     void Follow()
     {
-        Vector3 currentPos = target.position;
+        Vector3 currentPos = transform.position;
         Vector3 targetPos = target.position - distance;
 
         transform.position = Vector3.Lerp(currentPos, targetPos, followSpeed * Time.deltaTime);
     }
-    
 }

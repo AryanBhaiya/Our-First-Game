17d4ab4b23546376d6a70f7f52932edb0559e80d
AryanBhaiya <128392783+AryanBhaiya@users.noreply.github.com>
2023-06-25 15:18:19 +0530

    gv

--- Assets/Scripts/Platform.cs
+++ Assets/Scripts/Platform.cs
@@ -4,10 +4,26 @@ using UnityEngine;
 
 public class Platform : MonoBehaviour
 {
+
+    public GameObject platformblast;
+
+    public GameObject diamond, star;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        int randNumber = Random.Range(1, 21);
+        Vector3 tempPos = transform.position;
+        tempPos.y += 1.2f;
+        if (randNumber<4)
+        {
+            Instantiate(star, tempPos, star.transform.rotation);
+        }
+        if (randNumber == 7)
+        {
+            Instantiate(diamond, tempPos, diamond.transform.rotation);
+        }
+
     }//start
 
 
@@ -21,6 +37,7 @@ public class Platform : MonoBehaviour
 
     void FallDown()
     {
+        Instantiate(platformblast, transform.position, Quaternion.identity);
         GetComponent<Rigidbody>().isKinematic = false;
         Destroy(gameObject, 0.5f);
     }
---
+++ Assets/Scripts/itemscripts.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class itemscripts : MonoBehaviour
+{
+    public GameObject starblast;
+
+    public GameObject diamondblast;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(0f, 0f, 100f)*Time.deltaTime);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            if (gameObject.tag == "star")
+            {
+                Instantiate(starblast, transform.position, Quaternion.identity);
+            }
+            if (gameObject.tag == "diamond")
+            {
+                Instantiate(diamondblast, transform.position, Quaternion.identity);
+            }
+            Destroy(gameObject);
+        }
+    }
+}
